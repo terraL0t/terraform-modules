1ff8b75785e59920bfe36bd1f6c0ae5bf7e5ca89
Mikalai Rudzenka <mikalai.rudzenka@finteqhub.com>
2023-09-20 18:08:25 +0200

    set verson in main.tf

--- gcs/main.tf
+++ gcs/main.tf
@@ -1,6 +1,6 @@
 module "gcs_buckets" {
   source  = "terraform-google-modules/cloud-storage/google"
-  version = var.version
+  version = "~> 4.0"
   project_id  = var.project_id
   names = var.names
   prefix = var.prefix
