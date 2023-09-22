e905c12cd02ea4b3f3ab9fd116cc359f4fe8d155
Mikalai Rudzenka <mikalai.rudzenka@finteqhub.com>
2023-09-22 19:46:51 +0200

    enable compute API first

--- google_compute_shared_vpc_host_project/main.tf
+++ google_compute_shared_vpc_host_project/main.tf
@@ -1,3 +1,17 @@
-resource "google_compute_shared_vpc_host_project" "host" {
+resource "google_project_service" "compute_api" {
   project = var.host-project-id
+  service = "compute.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_dependent_services = true
+}
+
+resource "google_compute_shared_vpc_host_project" "host_project" {
+  project = var.host-project-id
+  
+  depends_on = [compute_api]
 }
