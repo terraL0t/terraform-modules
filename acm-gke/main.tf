e0c9da700c7ee01787a4328bf4a0d0fe593c39f8
Mikalai Rudzenka <mikalai.rudzenka@finteqhub.com>
2023-09-29 18:10:19 +0200

    set acm_version as a variable

--- acm-gke/main.tf
+++ acm-gke/main.tf
@@ -41,7 +41,7 @@ resource "google_gke_hub_feature_membership" "feature_member" {
   feature    = "configmanagement"
   membership = google_gke_hub_membership.membership.membership_id
   configmanagement {
-    version = "1.8.0"
+    version = var.acm_version
     config_sync {
       source_format = "unstructured"
       git {
